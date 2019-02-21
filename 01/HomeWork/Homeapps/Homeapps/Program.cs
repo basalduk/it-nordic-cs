b4bb5f316b4300624b3d6012b3d794a7f1349117
basalduk <47829188+basalduk@users.noreply.github.com>
2019-02-21 22:49:52 +0300

    Home work of the 1st lesson added

---
+++ 01/HomeWork/Homeapps/Homeapps/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Homeapps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Приветствую, {name}!");
+            Thread.Sleep(5000);
+            Console.WriteLine($"До скорых встреч, {name}!");
+            Console.ReadKey();
+        }
+    }
+}
