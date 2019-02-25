ae24a09bae49d8cdef9399b8345171ba3ae49553
Mikhail Basalduk <basmal@yandex.ru>
2019-02-25 21:48:26 +0300

    Lesson 2 ClassWork

---
+++ 02/ClassWork/ClassWorkapps/ClassWorkapps/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassWorkapps
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			Console.WriteLine("Введите радиус: ");
+			float r = float.Parse(Console.ReadLine());
+			float result = (float)Math.PI * r * r;
+			Console.WriteLine("площадь круга равна: {0}", result);
+
+
+		}
+	}
+}
