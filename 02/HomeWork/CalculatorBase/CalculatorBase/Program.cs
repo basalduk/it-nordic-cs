275c175c73fdb58adf729bbfeab1c1d88a894c74
basalduk <47829188+basalduk@users.noreply.github.com>
2019-02-26 18:54:29 +0300

    Lesson 2 HomeWork Calculator

---
+++ 02/HomeWork/CalculatorBase/CalculatorBase/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CalculatorBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            float b = float.Parse(Console.ReadLine());
+            float result1 = a + b;
+            float result2 = a - b;
+            float result3 = a * b;
+            Console.WriteLine("Результаты математических вычислений: ");
+            Console.WriteLine("a + b = {0}", result1);
+            Console.WriteLine("a - b = {0}", result2);
+            Console.WriteLine("a * b = {0}", result3);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02/HomeWork/CalculatorPro/CalculatorPro/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CalculatorPro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            float b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите одно из предложенных математических действий (+, -, *, /, %, ^): ");
+            char x = Convert.ToChar(Console.ReadLine());
+
+            switch (x) // решение с помощью оператора Switch
+            {
+                case '+': Console.WriteLine("Результат математичекого вычисления a + b = {0}", a + b);
+                    break;
+                case '-': Console.WriteLine("Результат математичекого вычисления a - b = {0}", a - b);
+                    break;
+                case '*': Console.WriteLine("Результат математичекого вычисления a * b = {0}", a * b);
+                    break;
+                case '/': Console.WriteLine("Результат математичекого вычисления a / b = {0}", a / b);
+                    break;
+                case '%': Console.WriteLine("Результат математичекого вычисления a % b = {0}", a % b);
+                    break;
+                case '^': Console.WriteLine("Результат математичекого вычисления a ^ b = {0}", +Math.Pow(a, b));
+                    break;
+            }
+
+
+
+            //if (x == '+') // решение с помощью оператора if
+            //    Console.WriteLine("Результат математичекого вычисления a + b = {0}", a + b);
+            //else if (x == '-')
+            //    Console.WriteLine("Результат математичекого вычисления a - b = {0}", a - b);
+            //else if (x == '*')
+            //    Console.WriteLine("Результат математичекого вычисления a * b = {0}", a * b);
+            //else if (x == '/')
+            //    Console.WriteLine("Результат математичекого вычисления a / b = {0}", a / b);
+            //else if (x == '%')
+            //    Console.WriteLine("Результат математичекого вычисления a % b = {0}", a % b);
+            //else if (x == '^')
+            //    Console.WriteLine("Результат математичекого вычисления a ^ b = {0}", +Math.Pow(a, b));
+
+
+            Console.ReadKey();
+        }
+    }
+}
