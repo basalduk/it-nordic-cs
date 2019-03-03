5f7e78572df32f6769d23119d7985aee3df75bfc
basalduk <47829188+basalduk@users.noreply.github.com>
2019-03-03 13:15:13 +0300

    Lesson 3 HomeWork NameAge

---
+++ 03/HomeWork/HomeWorkNameAge/HomeWorkNameAge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HomeWorkNameAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = new string[3];
+            var age = new string[3];
+            Console.WriteLine("Enter the names of 3 people: ");
+            for (int i = 0; i < name.Length; i++)
+            {
+                name[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Enter the age of these people: ");
+            for (int i = 0; i < age.Length; i++)
+            {
+                age[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine($"NAME: {name[i]}, age in 4 years: {age[i]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
