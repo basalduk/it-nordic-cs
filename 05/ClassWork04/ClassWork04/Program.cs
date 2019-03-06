5ae72bf3a3b5d469e8c0333522f817461c74ebad
Mikhail Basalduk <basmal@yandex.ru>
2019-03-06 21:36:59 +0300

    classWork 5

---
+++ 05/ClassWork04/ClassWork04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassWork04
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.WriteLine("Введите число");
+				int i = Int32.Parse(Console.ReadLine());
+
+				Console.WriteLine($"Число {i} " +
+					(i >= 0
+					? "положительное"
+					: "отрицательное"));
+
+			}
+			catch (Exception e)
+			{
+
+				Console.WriteLine($"{e.GetType()}, {e.Message}");
+				throw;
+			}
+			finally
+			{
+				Console.WriteLine("Введены неверные данные");
+			}
+		}
+	}
+}
