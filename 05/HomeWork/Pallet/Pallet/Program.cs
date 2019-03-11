f37c13cde9e7315be6137fd6eb80e9f3359ee3fb
basalduk <47829188+basalduk@users.noreply.github.com>
2019-03-11 15:12:16 +0300

    Lesson 5 HomeWorkPallet

---
+++ 05/HomeWork/Pallet/Pallet/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Pallet
+{
+    class Program
+    {
+        [Flags]
+
+        enum collor : byte
+        {
+            Non = 0x0,
+            Blue=0x1,
+            Cyan = 0x1 << 1,
+            Grey = 0x1 << 2,
+            Green = 0x1 << 3,
+            Magenta = 0x1 << 4,
+            Red = 0x1 << 5,
+            White = 0x1 << 6,
+            Yellow = 0x1 << 7,
+        }
+        static void Main(string[] args)
+        {
+            
+
+                Console.WriteLine("Введите цвет №1 (Blue, Cyan, Grey, Green, Magenta, Red, White, Yellow):");
+                collor LikeCollor = (collor)Enum.Parse(typeof(collor), Console.ReadLine());
+
+                Console.WriteLine("Введите цвет №2 (Blue, Cyan, Grey, Green, Magenta, Red, White, Yellow):");
+                collor LikeCollor2 =  LikeCollor | (collor)Enum.Parse(typeof(collor), Console.ReadLine());
+
+                Console.WriteLine("Введите цвет №3 (Blue, Cyan, Grey, Green, Magenta, Red, White, Yellow):");
+                collor LikeCollor3 = LikeCollor2 | (collor)Enum.Parse(typeof(collor), Console.ReadLine());
+
+                Console.WriteLine("Введите цвет №4 (Blue, Cyan, Grey, Green, Magenta, Red, White, Yellow):");
+                collor LikeCollor4 = LikeCollor3 | (collor)Enum.Parse(typeof(collor), Console.ReadLine());
+
+
+                Console.WriteLine($"Избранные цвета: {LikeCollor4}");
+                collor unLike = ~LikeCollor4;
+                Console.WriteLine($"Цвета невошедшие в Избранное: {unLike}");
+
+
+
+        }
+    }
+}
