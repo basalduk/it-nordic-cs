a6ce33fb16be262a6fd88f78c86371476b290faf
basalduk <47829188+basalduk@users.noreply.github.com>
2019-03-13 00:09:54 +0300

    Lesson 04 HomeWork Water

--- 04/HomeWork/water/water/Program.cs
+++ 04/HomeWork/water/water/Program.cs
@@ -13,27 +13,39 @@ namespace water
             large = 0x1 << 2,
         }
         static void Main(string[] args)
-        { 
-
-            Console.WriteLine("Какой объем сока требуется упаковать?");
-            float V = float.Parse(Console.ReadLine());
-            
-            var usedPackageTypes = PackageSizeType.None;
-
-            if (V > 0)
+        {
+            while (true)
             {
+                Console.WriteLine("Какой объем сока требуется упаковать?");
+                float V = float.Parse(Console.ReadLine());
 
-                usedPackageTypes = usedPackageTypes | PackageSizeType.large;
+                var usedPackageTypes = PackageSizeType.None;
 
-                bool isSmallNonUsed = (usedPackageTypes & PackageSizeType.small)== PackageSizeType.small;
-                bool isMediumNonUsed = (usedPackageTypes & PackageSizeType.medium) == PackageSizeType.medium;
-                bool isLargeNonUsed = (usedPackageTypes & PackageSizeType.large) == PackageSizeType.large;
+                if (V > 0)
+                {
+                    float valueLarge = (float)Math.Floor(V / 20);
+                    float valueMedium = (float)Math.Floor((V % 20) / 5);
+                    float valueSmall = (float)Math.Ceiling(((V % 20) % 5));
+
+                    if (valueLarge > 0) usedPackageTypes = usedPackageTypes | PackageSizeType.large;
+                    if (valueMedium > 0) usedPackageTypes = usedPackageTypes | PackageSizeType.medium;
+                    if (valueSmall > 0) usedPackageTypes = usedPackageTypes | PackageSizeType.small;
+
+                    bool isSmallUsed = (usedPackageTypes & PackageSizeType.small) == PackageSizeType.small;
+                    bool isMediumUsed = (usedPackageTypes & PackageSizeType.medium) == PackageSizeType.medium;
+                    bool isLargeUsed = (usedPackageTypes & PackageSizeType.large) == PackageSizeType.large;
+
+                    Console.WriteLine("Вам потребуются следующие контейнеры: ");
+
+                    if (isLargeUsed) Console.WriteLine($"20 л: {valueLarge} шт.");
+                    if (isMediumUsed) Console.WriteLine($" 5 л: {valueMedium} шт.");
+                    if (isSmallUsed) Console.WriteLine($" 1 л: {valueSmall} шт.");
+                }
+                else
+                {
+                    Console.WriteLine("Указан некорректный объем");
+                }
             }
-            else
-            {
-                Console.WriteLine("Указан некорректный объем");
-            }
-
         }
     }
 }
