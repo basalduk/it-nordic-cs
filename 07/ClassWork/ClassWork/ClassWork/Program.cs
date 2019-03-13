bcf39a10f50a900213d40c736af82a0c248b4478
Mikhail Basalduk <basmal@yandex.ru>
2019-03-13 21:39:26 +0300

    classWork 7

---
+++ 07/ClassWork/ClassWork/ClassWork/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassWork
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Console.WriteLine("Введите 2 дробных числа");
+			//float a = float.Parse(Console.ReadLine());
+			//float b = float.Parse(Console.ReadLine());
+			//float c = a + b;
+			//float d = a - b;
+			//float e = a * b;
+
+
+			//Console.WriteLine(a.ToString("0.00") + "+" + b.ToString("0.00")+ "=" + c.ToString("0.00"));
+
+			//Console.WriteLine("{0:0.00} - {1:0.00} = {2:0.00}", a,b,c);
+
+			//Console.WriteLine($"{a:0.00}*{b:0.00}={e:0.00}");
+
+			//var test = "авпьлвкпьвлдкпьтвлдкрьтп";
+
+			//Console.WriteLine(test.Substring(8, 20));
+
+
+			String text = ""
+
+
+		}
+	}
+}
