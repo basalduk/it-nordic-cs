3e2a5c3238a2543e2af24e8725015adcb3d8c3f7
primgerl <primgerl@MacBook-Pro-primgerl.local>
2019-03-17 21:12:52 +0300

    Lesson 06 HomeWork #1

---
+++ 06/HomeWork/Chet/Chet/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Chet
+{
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+           
+             while (true)
+            {
+                Console.WriteLine("Введите положительное натуральное число не более 2 миллиардов: ");
+                string a = Console.ReadLine();
+                int result = 0;
+
+                try
+                {
+                    if (Int64.Parse(a) > 0 & Int64.Parse(a) < 2000000000)
+                    {
+
+                        foreach (int t in a)
+                        {
+                            if (t % 2 == 0) result++;
+                        }
+
+                        Console.WriteLine($"В числе {a} содержится следующее количество четных цифр: {result} ");
+
+                    }
+                    else
+                    {
+                        throw new OverflowException();
+                    }
+                }
+                catch (FormatException ex1)
+                {
+                    Console.WriteLine("Ошибка {0} Попробуйте еще раз: ", ex1.GetType());
+                }
+
+                catch (OverflowException ex2)
+                {
+                    Console.WriteLine("Ошибка {0} Попробуйте еще раз: ",ex2.GetType());
+                }
+            }
+        }
+    }
+}
