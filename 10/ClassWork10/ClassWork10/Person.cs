5c004b344b431cf9d046f528948a21314d845bb1
Mikhail Basalduk <basmal@yandex.ru>
2019-03-25 21:33:37 +0300

    classWork 10

---
+++ 10/ClassWork10/ClassWork10/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassWork10
+{
+	public class Pet
+	{
+		public string Kind;
+		public string Name;
+		public string Sex;
+		public int Age;
+		//{
+		//	get
+		//	{
+		//		return age;
+		//	}
+		//	set
+		//	{
+		//		if (value > 0 && value < 140)
+		//		{
+		//			age = value;
+		//		}
+		//	}
+
+		public void WritePropertiesString()
+		{
+			
+		}
+	}
+	
+
+	
+}
---
+++ 10/ClassWork10/ClassWork10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ClassWork10
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pet pet1 = new Pet
+			{
+			Kind = "животное",
+			Name = "кличка",
+			Sex = "male",
+			Age = 10,
+			};
+
+			Pet pet2 = new Pet();
+			pet2.Kind = "животное";
+			pet2.Name = "кличка";
+			pet2.Sex = "male";
+			pet2.Age = 10;
+			
+
+			Console.WriteLine("Kind : {0}, Name: {1}, Sex{2}, Age : {3})", pet1.Kind,pet1.Name,pet1.Sex,pet1.Age);
+			Console.WriteLine("Kind : {0}, Name: {1}, Sex{2}, Age : {3})", pet2.Kind, pet2.Name, pet2.Sex, pet2.Age);
+
+		}
+	}
+
+}
