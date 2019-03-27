0f261616932f6b8084140aeabeb8dc772e88202a
Mikhail Basalduk <basmal@yandex.ru>
2019-03-27 21:36:13 +0300

    classWork 11

---
+++ 11/ClassWork11/ClassWork11/ClassWork11/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassWork11
+{
+
+	public class Pet
+	{
+		public string Kind;
+		public string Name;
+		public string Sex;
+		public DateTimeOffset DateOfBirh;
+
+		public int Age
+		{
+			get
+			{
+				TimeSpan age = DateTimeOffset.UtcNow.Subtract(DateOfBirh);
+				return Convert.ToInt32(Math.Floor(age.TotalDays / 365.242));
+			}
+
+		}
+		public string Description
+		{
+			get { return $"{Name} is a {Kind} ({Sex}) of {Age} years old"; }
+		}
+
+		public string ShortDescription
+		{
+			get { return $"{Name} is a {Kind}"; }
+		}
+		public void WriteDescription(bool showFullDescription = false)
+		{
+				Console.WriteLine(showFullDescription
+					? Description
+					: ShortDescription);
+			}
+		}
+
+	}
+}
+
---
+++ 11/ClassWork11/ClassWork11/ClassWork11/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassWork11
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pet pet1 = new Pet
+			{
+				Kind = "Котенок",
+				Name = "кыс-кыс",
+				Sex = "male",
+				DateOfBirh = DateTimeOffset.Parse("2018-01-01"),
+			};
+			pet1.WriteDescription(false);
+
+			Pet pet2 = new Pet();
+			pet2.Kind = "Собака";
+			pet2.Name = "Шарик";
+			pet2.Sex = "male";
+			pet2.DateOfBirh = DateTimeOffset.Parse("2018-01-01");
+			pet2.WriteDescription(true);
+
+
+			Console.WriteLine("Kind : {0}, Name: {1}, Sex{2}, Age : {3})", pet1.Kind, pet1.Name, pet1.Sex, pet1.Age);
+			Console.WriteLine("Kind : {0}, Name: {1}, Sex{2}, Age : {3})", pet2.Kind, pet2.Name, pet2.Sex, pet2.Age);
+
+
+
+		}
+		//static void Main(string[] args)
+		//{
+
+		//	Random rand = new Random();
+		//	int r = rand.Next();
+		//	Console.WriteLine("Hello World!");
+		//}
+		//public string PropertiesString 
+		//{
+		//	get { return "выапавп"; }
+		//}
+
+		//public string PropertiesStringmetod()
+		//{
+		//	 return "выапавп";
+		//}
+
+
+	}
+}
