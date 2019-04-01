10e55d171d8220f4cbdf40703cce14ecdd21bdfe
basalduk <47829188+basalduk@users.noreply.github.com>
2019-04-01 16:50:56 +0300

    HalfHomeWork11

---
+++ 11/HomeWork11/morning/morning/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace morning
+{
+    public class ReminderItem
+    {
+        public DateTimeOffset AlarmDate;
+        public string AlarmMessage;
+        public TimeSpan TimeToAlarm
+        {
+            get
+            {
+                TimeSpan timeToAlarm = DateTimeOffset.UtcNow.Subtract(AlarmDate);
+                return timeToAlarm;
+            }
+        }
+
+        public bool IsOutdated ()
+        {
+            return true;
+        }
+        public string WriteProperties
+        {
+            get
+            {
+                return $"AlarmDate: {AlarmDate}\n AlarmMessage: {AlarmMessage}\n TimeToAlarm: {TimeToAlarm}\n IsOutdated: {IsOutdated}";
+            }
+        }
+
+    }
+}
---
+++ 11/HomeWork11/morning/morning/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace morning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReminderItem reminderItem1 = new ReminderItem
+            {
+                AlarmDate = DateTimeOffset.Parse("01/04/2019 08:00:00"),
+                AlarmMessage = "morning"
+            };
+            ReminderItem reminderItem2 = new ReminderItem
+            {
+                AlarmDate = DateTimeOffset.Parse("01/04/2019 14:00:00"),
+                AlarmMessage = "dinner"
+            };
+            
+
+        }
+
+
+    }
+
+
+}
