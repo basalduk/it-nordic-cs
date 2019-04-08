604ae615364b94c4ae0699f0ded0bd376dcc989e
Mikhail Basalduk <basmal@yandex.ru>
2019-04-08 21:29:36 +0300

    classWork 14

---
+++ 14/ClassWork14/ClassWork14/ClassWork14/ErrorList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassWork14
+{
+	class ErrorList : IDisposable, IEnumerable<string>
+	{
+
+		public static string OutputPrefixFormat()
+		{
+			return "mmmm dd, yyyy (hh:mm tt)";
+		}
+		public void WriteToConsole()
+		{
+
+			DateTimeOffset data = DateTimeOffset.Now.ToString(OutputPrefixFormat);
+		}
+
+
+
+		public string Category { get; private set; }
+		private List<string> _errors;
+
+
+		public ErrorList(string category)
+		{
+			_errors = new List<string>();
+			Category = category;
+		}
+
+		public void Dispose()
+		{
+			if (_errors != null)
+				_errors.Clear();
+				_errors = null;
+		}
+
+		public void Add(string errorMesege)
+		{
+			_errors.Add(errorMesege);
+		}
+
+		public IEnumerator<string> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ 14/ClassWork14/ClassWork14/ClassWork14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassWork14
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using(var errorList= new ErrorList("categoryes"))
+			{
+				errorList.Add("IO error");
+				errorList.Add("Some unknown error");
+				errorList.Add("dasfsa");
+
+				foreach (string error in errorList)
+				{
+					Console.WriteLine("{0} {1}",errorList.WriteToConsole, errorList.Category);
+				}
+			}
+		}
+	}
+}
