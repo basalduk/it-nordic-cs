659426bb2227293c1b9a00d256cc0be85c44e764
basalduk <47829188+basalduk@users.noreply.github.com>
2019-04-22 16:59:32 +0300

    ChatBot

---
+++ ChatBot/ChatBot/IReminderSender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatBot
+{
+    class IReminderSender
+    {
+        //ReminderItem[] ;
+        public string GetReminderItemsToSend()
+        {
+            return "12";
+        }
+        public void Sender()
+        { }
+    }
+}
---
+++ ChatBot/ChatBot/ReminderItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatBot
+{
+    class ReminderItem
+    {
+        public Guid Id { get; }
+        public DateTimeOffset AlarmDate { get; set; }
+        public string Message { get; set; }
+        public string ContactId { get; set; }
+        public bool IsOutdated { get; }
+        public string Property()
+        {
+            return " ";
+        }
+    }
+}
