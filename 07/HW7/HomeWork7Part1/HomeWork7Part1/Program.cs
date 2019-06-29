6e8d9f19131b694989bcc66ac002cb8eb78fa65b
basalduk <basmal@yandex.ru>
2019-06-29 21:14:21 +0300

    HomeWork7

---
+++ 07/HW7/HomeWork7Part1/HomeWork7Part1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HomeWork7Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your string:");
+
+            string yourString;
+            string[] summWord;
+            int summA = 0;
+
+            while (true)
+            {
+                yourString = Console.ReadLine();
+
+                summWord = yourString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (summWord.Length >= 2)
+                    break;
+
+                Console.WriteLine("Enter at least 2 words, try again");
+            }
+
+            foreach (string s in summWord)
+            {
+                if (s[0] == 'A' || s[0] == 'a' || s[0] == 'А' || s[0] == 'а') //Русские и Английские буквы
+                    summA += 1;
+            }
+
+            Console.WriteLine($"Number of words starting with the letter 'A': {summA}\n" +
+                              $"Press Enykey");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 07/HW7/HomeWork7Part1/HomeWork7Part2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HomeWork7Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your string:");
+
+            string yourString;
+            string[] arrayWord;
+            string yourStringConverted = "";
+
+            while (true)
+            {
+                yourString = Console.ReadLine();
+
+                arrayWord = yourString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (arrayWord.Length >= 1)
+                    break;
+
+                Console.WriteLine("Your string is empty, try again");
+            }
+
+
+            for (int i = yourString.Length - 1; i >= 0; --i)
+            {
+                yourStringConverted += (yourString.ToLower())[i];
+            }
+
+
+            Console.WriteLine($"Your string converted: {yourStringConverted}\n" +
+                              $"Press Enykey");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
