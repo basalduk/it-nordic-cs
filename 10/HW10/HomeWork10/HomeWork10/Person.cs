03e92c00f76b9c609c55d4ae456f558f2fc05d43
basalduk <basmal@yandex.ru>
2019-06-29 23:48:40 +0300

    HomeWork10

---
+++ 10/HW10/HomeWork10/HomeWork10/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork10
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public byte Age { get; set; }
+
+        public byte FutureAge
+        {
+            get
+            {
+                return (byte)(Age + 4);
+            }
+        }
+
+        public string PersonString
+        {
+            get
+            {
+                return $"Name: {Name}, age in 4 years: {FutureAge}";
+            }
+        }
+    }
+}
---
+++ 10/HW10/HomeWork10/HomeWork10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HomeWork10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] arrayPerson = new Person[3];
+
+            for (int i = 0; i < 3; ++i)
+            {
+                arrayPerson[i] = new Person(); 
+
+                Console.WriteLine($"Enter name {i}:");
+                arrayPerson[i].Name = Console.ReadLine();
+
+                Console.WriteLine($"Enter Age {i}:");
+                arrayPerson[i].Age =  Convert.ToByte(Console.ReadLine());
+
+            }
+
+            for (int i = 0; i < 3; ++i)
+            {
+                Console.WriteLine(arrayPerson[i].PersonString);
+            }
+
+            Console.WriteLine("Press any key to continue..");
+            Console.ReadKey();
+        }
+    }
+}
